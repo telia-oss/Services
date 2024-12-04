8e764d11b88034e2f8155f3b38c220e448596f4d
Jyoti Bharankar <jyoti.bharankar@capgemini.com>
2024-12-04 16:18:44 +0100

    Add files via upload

---
+++ LineOrder/PROD/ans13.sql
@@ -0,0 +1,28 @@
+--ans13
+SELECT   PERSN_REF, ORDER_NM_TXT FROM     STHDSN1.TICGRP1.CNUM_HIST_DATA WHERE    CURR_CUST_PARTN_ID=:CURR_CUST_PARTN_ID AND      CNUM_OID =:CNUM_OID AND      INTVL_START_DATE = (SELECT MAX(INTVL_START_DATE) FROM STHDSN1.TICGRP1.CNUM_HIST_DATA WHERE CURR_CUST_PARTN_ID =:CURR_CUST_PARTN_ID AND CNUM_OID =:CNUM_OID)
+
+
+
+
+
+
+{
+  "dburl": "http://131.116.44.152:1317/services/DB2ServiceManager",
+  "username": "a472inb",
+  "password": "INblatS2",
+  "sqlStmt": "SELECT   PERSN_REF, ORDER_NM_TXT FROM     STHDSN1.TICGRP1.CNUM_HIST_DATA WHERE    CURR_CUST_PARTN_ID=:CURR_CUST_PARTN_ID AND      CNUM_OID =:CNUM_OID AND      INTVL_START_DATE = (SELECT MAX(INTVL_START_DATE) FROM STHDSN1.TICGRP1.CNUM_HIST_DATA WHERE CURR_CUST_PARTN_ID =:CURR_CUST_PARTN_ID AND CNUM_OID =:CNUM_OID)",
+  "collectionID": "SYSIBMSERVICE",
+  "serviceName": "ans13",
+  "owner": "COMINAT5",
+  "validate": "BIND",
+  "description": "ans13 based on CNUM_OID and CURR_CUST_PARTN_ID",
+  "requestType": "createService",
+  "version": "V1",
+  "response": ""
+}
+
+
+
+curl -X GET http://131.116.44.152:1317/services/SYSIBMSERVICE/ans13/V1 -H "Content-Type: application/json" -u a472inb:INblatS2 | jq
+
+
