0d89ab8deb1b735e6ed0ba4ab481b3cdea5fca13
Jyoti Bharankar <jyoti.bharankar@capgemini.com>
2024-12-04 16:26:08 +0100

    Add files via upload

---
+++ SearchKostKund/sos04.sql
@@ -0,0 +1,16 @@
+{
+  "dburl": "http://131.116.44.144:1316/services/DB2ServiceManager",
+  "username": "a472inb",
+  "password": "INblatS2",
+  "sqlStmt": "SELECT  DISTINCT CAST(BACCT_ID AS VARCHAR(15) FOR SBCS DATA) AS BACCT_ID,CURR_CUST_OID,CURR_CUST_PARTN_ID FROM  STHDSN6.TENGRA2.BACCT_ID_HIST_DATA WHERE CAST(BACCT_ID  AS VARCHAR(15) FOR SBCS DATA) =:BACCT_ID",
+  "collectionID": "SYSIBMSERVICE",
+"owner": "COMINAT5",
+  "validate": "BIND",
+  "serviceName": "sos04",
+  "description": "sos04 with BACCT_ID",
+  "requestType": "createService",
+  "version": "V1",
+  "response": ""
+}
+
+
