ac9fa25250501e2be91e11db2ea0f5dcd367217e
Jyoti Bharankar <jyoti.bharankar@capgemini.com>
2024-12-04 16:12:41 +0100

    Add files via upload

---
+++ BillingAccount/PROD/fks05.sql
@@ -0,0 +1,28 @@
+--fks05
+SELECT CAST(A.INV_ID AS VARCHAR(14) FOR SBCS DATA) AS  INV_ID,CAST(A.PAID_IND AS VARCHAR(14) FOR SBCS DATA) AS PAID_IND, A.INV_DATE, A.INV_PAID_DATE FROM  STHDSN1.TICGRP1.INV_DATA A WHERE  A.BACCT_PARTN_ID =:BACCT_PARTN_ID AND    A.BACCT_OID =:BACCT_OID ORDER BY A.INV_ID DESC
+
+
+
+
+
+
+
+{
+  "dburl": "http://131.116.44.152:1317/services/DB2ServiceManager",
+  "username": "a472inb",
+  "password": "INblatS2",
+  "sqlStmt": "SELECT CAST(A.INV_ID AS VARCHAR(14) FOR SBCS DATA) AS  INV_ID,CAST(A.PAID_IND AS VARCHAR(14) FOR SBCS DATA) AS PAID_IND, A.INV_DATE, A.INV_PAID_DATE FROM  STHDSN1.TICGRP1.INV_DATA A WHERE  A.BACCT_PARTN_ID =:BACCT_PARTN_ID AND    A.BACCT_OID =:BACCT_OID ORDER BY A.INV_ID DESC",
+  "collectionID": "SYSIBMSERVICE",
+  "serviceName": "fks05",
+  "owner": "COMINAT5",
+    "validate": "BIND",
+  "description": "fks05 based on  BACCT_OID and BACCT_PARTN_ID ",
+  "requestType": "createService",
+  "version": "V1",
+  "response": ""
+}
+
+
+
+curl -X GET http://131.116.44.152:1317/services/SYSIBMSERVICE/fks05/V1 -H "Content-Type: application/json" -u a472inb:INblatS2 | jq
+
---
+++ BillingAccount/PROD/fks06.sql
@@ -0,0 +1,29 @@
+--fks06
+SELECT CAST(A.PAID_IND AS VARCHAR(14) FOR SBCS DATA) AS PAID_IND, A.INV_DATE, A.INV_PAID_DATE,CAST( A.INV_ID  AS VARCHAR(14) FOR SBCS DATA) AS INV_ID FROM  STHDSN1.TICGRP1.INV_DATA A WHERE  CAST( A.INV_ID  AS VARCHAR(14) FOR SBCS DATA) =:INV_ID ORDER BY A.INV_ID DESC
+
+
+
+
+
+
+
+
+{
+  "dburl": "http://131.116.44.152:1317/services/DB2ServiceManager",
+  "username": "a472inb",
+  "password": "INblatS2",
+  "sqlStmt": "SELECT CAST(A.PAID_IND AS VARCHAR(14) FOR SBCS DATA) AS PAID_IND, A.INV_DATE, A.INV_PAID_DATE,CAST( A.INV_ID  AS VARCHAR(14) FOR SBCS DATA) AS INV_ID FROM  STHDSN1.TICGRP1.INV_DATA A WHERE  CAST( A.INV_ID  AS VARCHAR(14) FOR SBCS DATA) =:INV_ID ORDER BY A.INV_ID DESC",
+  "collectionID": "SYSIBMSERVICE",
+  "serviceName": "fks06",
+  "owner": "COMINAT5",
+   "validate": "BIND",
+  "description": "fks06 based on  INV_ID ",
+  "requestType": "createService",
+  "version": "V1",
+  "response": ""
+}
+
+
+
+curl -X GET http://131.116.44.152:1317/services/SYSIBMSERVICE/fks06/V1 -H "Content-Type: application/json" -u a472inb:INblatS2 | jq
+
